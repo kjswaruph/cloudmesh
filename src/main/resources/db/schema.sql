315146631aae53b5a2deedef16e4702e024e1a7b
Swaruph Kolli <jyothiswaruphkolli@gmail.com>
2025-10-09 16:09:30 +0530

    add schemas

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,3 +1,44 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE users(
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
-)
+DROP TABLE IF EXISTS projects;
+CREATE TABLE projects(
+    project_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    project_name VARCHAR(255) NOT NULL,
+    description TEXT,
+    user_id UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_project_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS resources;
+CREATE TABLE resources(
+    resource_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    resource_name VARCHAR(255),
+    project_id UUID NOT NULL ,
+    provider VARCHAR(50) NOT NULL ,
+    resource_type VARCHAR(50) NOT NULL ,
+    resource_status VARCHAR(50) NOT NULL ,
+    resource_region VARCHAR(100),
+    resource_cost NUMERIC(10, 2),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_resource_project
+        FOREIGN KEY (project_id)
+        REFERENCES projects(project_id)
+        ON DELETE CASCADE
+);
