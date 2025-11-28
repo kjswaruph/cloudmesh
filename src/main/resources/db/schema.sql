6729b4be9a9b6b8f9fac47c45fb3960c7364acc9
Swaruph Kolli <jyothiswaruphkolli@gmail.com>
2025-11-28 23:36:45 +0530

    add schema.sql

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,6 +1,10 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS cloud_credentials CASCADE;
+DROP TABLE IF EXISTS projects CASCADE;
+DROP TABLE IF EXISTS resources CASCADE;
+
 CREATE TABLE users(
-    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID PRIMARY KEY ,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     username VARCHAR(50) UNIQUE NOT NULL,
@@ -11,9 +15,8 @@ CREATE TABLE users(
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-DROP TABLE IF EXISTS projects;
 CREATE TABLE projects(
-    project_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    project_id UUID PRIMARY KEY,
     project_name VARCHAR(255) NOT NULL,
     description TEXT,
     user_id UUID NOT NULL,
@@ -25,10 +28,9 @@ CREATE TABLE projects(
         ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS resources;
 CREATE TABLE resources(
-    resource_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    resource_name VARCHAR(255),
+    resource_id UUID PRIMARY KEY,
+    resource_name VARCHAR(255) NOT NULL,
     project_id UUID NOT NULL ,
     provider VARCHAR(50) NOT NULL ,
     resource_type VARCHAR(50) NOT NULL ,
@@ -40,5 +42,19 @@ CREATE TABLE resources(
     CONSTRAINT fk_resource_project
         FOREIGN KEY (project_id)
         REFERENCES projects(project_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE cloud_credentials(
+    credential_id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    provider VARCHAR(50) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    provider_config JSONB NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_credential_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
         ON DELETE CASCADE
 );
